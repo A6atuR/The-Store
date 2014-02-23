823a749c795dbbe16fd24fa7c5191a726ab658e2
vMARKUSv <stoc2010@yandex.ru>
2014-02-24 01:00:09 +0200

    Create The Store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amazon::Application.config.secret_key_base = '1919893b30b0afdf635c0e619742b09dea3f8bd5e4fa93955a8df793af5927e398e5127bb81b8d54984e9ad2aaee95e9b1374e8c447ed9b535b1a1887e5301f7'
